adcb02aa2da1fc2a972275857f9c294ba216649a
Daniel Moraes Mariz <danielmrz.dev@gmail.com>
2025-02-05 12:24:30 -0300

    Servidor totalmente online e conectado ao banco

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ import { app } from "./src/app.js"
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}.`);    
+    console.log(`Servidor rodando! 🔥`);    
 })
