66c93c78a860503f7d82e3fa80543ad607025984
Daniel Moraes Mariz <danielmrz.dev@gmail.com>
2025-02-05 12:15:12 -0300

    Ajusta configuração do banco de dados

--- src/config/database-connect.js
+++ src/config/database-connect.js
@@ -2,11 +2,10 @@ import mongoose from "mongoose";
 
 export async function connectDatabase() {
     try {
-        await mongoose.connect(process.env.DB_CONNECTION_STRING, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        await mongoose.connect(process.env.DB_CONNECTION_STRING);
+        console.log("Conectado ao Mongo com sucesso! 🔥");
     } catch (error) {
+        console.log("Erro ao conectar ao MongoDB... ☹️");
         throw error;
     }
     return mongoose.connection;
