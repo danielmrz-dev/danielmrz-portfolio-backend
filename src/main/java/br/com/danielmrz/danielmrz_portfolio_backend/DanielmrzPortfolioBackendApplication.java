512e027632ef67a24da2c6c0577353d4b8504f23
Daniel Mariz <danielmrz.dev@gmail.com>
2026-06-26 10:45:57 -0300

    iniciando repo

---
+++ src/main/java/br/com/danielmrz/danielmrz_portfolio_backend/DanielmrzPortfolioBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.danielmrz.danielmrz_portfolio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanielmrzPortfolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanielmrzPortfolioBackendApplication.class, args);
+	}
+
+}
