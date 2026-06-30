17ab4e2e409f799dd8e94d98b729257ad8fd667c
Daniel Mariz <danielmrz.dev@gmail.com>
2026-06-30 15:42:02 -0300

    ajusta cors config

--- src/main/java/br/com/danielmrz/danielmrz_portfolio_backend/config/CorsConfiguration.java
+++ src/main/java/br/com/danielmrz/danielmrz_portfolio_backend/config/CorsConfiguration.java
@@ -9,7 +9,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-      .allowedOrigins("http://127.0.0.1:5501", "http://localhost:4200")
+      .allowedOrigins("https://danielmrz-portfolio-backend.onrender.com/")
       .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
   }
 }
